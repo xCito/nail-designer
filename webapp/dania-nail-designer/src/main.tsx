776289ec59f20573f68f7ef8db50b756836b216f
Juan L <juanlantiguajr@gmail.com>
2024-01-26 23:12:01 -0500

    Goal notes

--- webapp/dania-nail-designer/src/main.tsx
+++ webapp/dania-nail-designer/src/main.tsx
@@ -12,9 +12,41 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 )
 
 
-/**
- * GOALS
- * 
+/*
+GOALS
+  #1: Consultation will be like chat bot help instead of flat page w lists
+
+  #2:
+  (new) Consultation options -> Desired Service; Basic Manicure, 
+  Gel Manicure, Extensions/Enhancements, New Set, Take-Down Removal
+
+  Gel Manicure is Basic Manicure w BaseGel in “Foundation” ON. 
+  Base gel is same as Gel Manicure
+
+  #3:
+  Consultation; Removing prior design or set?, 
+  what is your “Desired Service”? show options, 
+  (add) “Desired Base” next using the bubbles from “Foundation”,
+  include manicure?, Is your desired service: xyz?, 
+  if yes; Consultation Complete button appears & move on to “Design”
+
+  #4:
+  Add “Add-Ons”; Gems, Charms, Extra Skin Smoothing 
+  or Dry Skin Treatment (or both? as individual services)
+
+  #5:
+  Manicure is $20 again 👀
+
+  #6:
+  If click “Take Down”, include manicure, base gel 
+  turns on but there are no pre or during service shaping options 
+
+  #7:
+  Put “Consultation”, “Foundation”, “Design” as tabs under the title & úña
+
+*/
+
+/*
  * [x] Form asking for clients current nail details:
  *      - LENGTH 
  *      - SHAPE
